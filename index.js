43ae8f1312a85cef495490915d792a4d8282127b
extremeheat <extreme@protonmail.ch>
2023-08-28 07:07:01 +0000

    add anvil chunk read/write

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const fs = require('fs')
 const nbt = require('prismarine-nbt')
+const RegionFile = require('./RegionFile')
 
 const usage = `usage, feel free to use natural language:
 Parse an NBT file to JSON: 
@@ -17,14 +18,14 @@ Write an JSON file to uncompressed NBT (defaults to big endian):
     nbt-dump write level.json to level.dat as little
 `
 
-function main(args, argsStr) {
+function main (args, argsStr) {
   if (args.length == 0 || argsStr.includes('help')) {
     console.warn(usage)
     process.exit(1)
   }
   if (args[0] == 'read') args.shift()
   let files = []
-  for (var arg of args) {
+  for (const arg of args) {
     if (arg.includes('.')) files.push(arg)
   }
   const getFmt = () => {
@@ -46,7 +47,8 @@ function main(args, argsStr) {
   console.warn(arguments)
 }
 
-function write(inpf, outf, fmt) {
+function write (inpf, outf, fmt) {
+  if (outf.endsWith('.mca')) return writeAnvilRegion(inpf, outf)
   console.log(`* Writing JSON from "${inpf}" to "${outf}" as ${fmt || 'big'} endian`)
 
   const json = JSON.parse(fs.readFileSync(inpf))
@@ -60,10 +62,10 @@ function write(inpf, outf, fmt) {
     console.warn('Failed to write. Make sure that the JSON schema matches the prismarine-nbt ProtoDef schema. See https://github.com/PrismarineJS/prismarine-nbt')
     throw e
   }
-  return
 }
 
-async function read(inpf, outf, fmt) {
+async function read (inpf, outf, fmt) {
+  if (inpf.endsWith('.mca')) return readAnvilRegion(inpf, outf)
   console.log(`* Dumping NBT file "${inpf}" to "${outf || 'stdout'}" as JSON ${fmt ? 'as ' + fmt : ''}`)
 
   const buffer = await fs.promises.readFile(inpf)
@@ -74,10 +76,51 @@ async function read(inpf, outf, fmt) {
     const json = JSON.stringify(parsed, (k, v) => typeof v === 'bigint' ? v.toString() : v)
     fs.writeFileSync(outf, json)
   } else {
-    console.log(nbt.simplify(parsed))
+    console.dir(nbt.simplify(parsed), { depth: null })
   }
 
   return true
+}
+
+async function writeAnvilRegion (inputFile, outputRegion) {
+  console.log(`* Writing JSON from "${inputFile}" to "${outputRegion}" as an Anvil-format chunk`)
+  const json = JSON.parse(fs.readFileSync(inputFile))
+  const region = new RegionFile(outputRegion)
+  await region.initialize()
+  for (const chunk of json) {
+    // console.dir(chunk.tag,{depth:null})
+    // const tag = nbt.writeUncompressed(chunk.tag, 'big')
+    await region.write(chunk.x, chunk.z, chunk.tag)
+  }
+  await region.close()
+}
+
+async function readAnvilRegion (inputFile, outputFile) {
+  console.log(`* Dumping region file "${inputFile}" to "${outputFile || 'stdout'}" as JSON`)
+  const region = new RegionFile(inputFile)
+  await region.initialize()
+  const data = []
+  const chunks = await region.readAll()
+  for (const [[x, z], chunk] of chunks) {
+    console.log(nbt.writeUncompressed(JSON.parse(JSON.stringify(chunk)), 'big'))
+    // const x = chunk.value.Level.value.xPos.value ?? chunk.value.xPos.value
+    // const z = chunk.value.Level.value.zPos.value ?? chunk.value.zPos.value
+    data.push({ x, z, tag: chunk })
+    if (!outputFile) {
+      const j = JSON.stringify(nbt.simplify(chunk), (k, v) => {
+        if (typeof v.valueOf() === 'bigint') return parseInt(v)
+        if (Array.isArray(v) && v.length > 20) {
+          return `(${v.length} collapsed entries)`
+        }
+        return v
+      })
+      const parsed = JSON.parse(j)
+      console.log('X:', parsed.Level.xPos || parsed.xPos, 'Z:', parsed.Level.zPos || parsed.zPos)
+      console.dir(parsed, { depth: null })
+    }
+  }
+  if (outputFile) fs.writeFileSync(outputFile, JSON.stringify(data))
+  await region.close()
 }
 
 const args = process.argv.slice(2)
